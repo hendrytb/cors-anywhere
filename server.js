1b9bf741e439be0f55c58761e86c4198240b583d
Achmad Gozali <gozali@gmail.com>
2018-05-14 17:09:47 +0700

    Remove unnecessary conf

--- server.js
+++ server.js
@@ -27,17 +27,10 @@ cors_proxy.createServer({
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-heroku-queue-wait-time',
-    'x-heroku-queue-depth',
-    'x-heroku-dynos-in-use',
-    'x-request-start',
   ],
   redirectSameOrigin: true,
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
+    xfwd: true,
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
